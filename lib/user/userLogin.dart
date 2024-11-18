be1a0c371705ca1ed1e4986248947648c5019497
AthithyanKannan <athithyan1314@gmail.com>
2024-11-18 11:29:12 +0530

    loginPage

---
+++ lib/user/userLogin.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_button.dart';
+import 'package:food_delivery_app/components/my_text_field.dart';
+
+class UserLogin extends StatefulWidget {
+  const UserLogin({Key? key}) : super(key: key);
+
+  @override
+  _UserLoginState createState() => _UserLoginState();
+}
+
+TextEditingController userName = TextEditingController();
+TextEditingController password = TextEditingController();
+
+void login() {}
+
+class _UserLoginState extends State<UserLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Welcome back you\'ve been missed",
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 25),
+              MyTextField(
+                controller: userName,
+                hintText: "UserName",
+                obscureText: false,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              MyTextField(
+                controller: password,
+                hintText: "Password",
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Text(
+                      "Forget password",
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              MyButton(onTap: login),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Text("Not a member"),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    "Register Now",
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
